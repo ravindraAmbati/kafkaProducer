5eb4b0284dd5748954b7071007f198124856fe1b
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-08-24 01:18:57 +0530

    enable SSL, closes #23

--- src/main/java/com/learn/kafkaProducer/config/AutoKafkaTopicCreationConfig.java
+++ src/main/java/com/learn/kafkaProducer/config/AutoKafkaTopicCreationConfig.java
@@ -3,11 +3,10 @@ package com.learn.kafkaProducer.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-@Profile("local")
+//@Profile("local")
 public class AutoKafkaTopicCreationConfig {
 
     @Bean
